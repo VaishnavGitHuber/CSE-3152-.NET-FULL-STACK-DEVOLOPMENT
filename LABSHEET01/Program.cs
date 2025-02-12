061c739015d06ec4e03df2b4509f85a60d195091
Vaishnav Krishna P <124416940+VaishnavGitHuber@users.noreply.github.com>
2025-02-12 20:10:38 +0530

    Two Labsheets Uploading

---
+++ LABSHEET01/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Labsheet 01: claculate the total salary of the employee 
+ provide the basic salary input using the console,
+ - 15% of basic is HRA
+ - 30% of basic is DA 
+ Hint: TS = BS + HRA + DA
+ */
+
+// Information : calaculate the salary of the employee(ie, BS + HRA + DA)
+// input: basic salary of the employee 
+// output: total salary of the employee 
+
+// input from the user 
+Console.Write("Enter the Salary: ");
+double BasicSalary = Convert.ToDouble(Console.ReadLine());
+
+// calculations 
+double HRA = 0.15 * BasicSalary;
+double DA = 0.3 * BasicSalary;
+double TotalSalary = BasicSalary + HRA + DA;
+
+// output 
+Console.WriteLine($"Total Salary of the employee: {TotalSalary}");
---
+++ LABSHEET02/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Write Code to award grade to the students.
+- Get name of the student and three student marks.
+- Claculate the total marks 
+- award Excellent, good, average grades
+Hint: 200-300 Excellent, 100-200 Good, less than 100 Average 
+*/
+
+// array to store the marks anf name of the students 
+string[] Name = new string[3];
+double[] Marks = new double[3];
+string[] Grade = new string[3];
+
+// input the marks and grade of the 3 students 
+for (int i = 0; i < Name.Length; i++)
+{
+    Console.Write($"Enter {i + 1} person's name: ");
+    Name[i] = Console.ReadLine();
+    Console.Write($"Enter {i + 1} person's mark: ");
+    Marks[i] = Convert.ToDouble(Console.ReadLine());
+
+    // logic using to store the award
+    if (Marks[i] > 200 && Marks[i] < 300)
+    {
+        Grade[i] = "Excellent";
+    }
+    else if (Marks[i] > 100 && Marks[i] < 200)
+    {
+        Grade[i] = "Good";
+    }
+    else if (Marks[i] < 100)
+    {
+        Grade[i] = "Average";
+    }
+}
+
+// award the grade 
+Console.WriteLine("\n");
+for (int i = 0; i < Name.Length; i++)
+{
+    Console.WriteLine($"{Name[i]} got {Marks[i]}:Grade is {Grade[i]}");
+}
+
+
