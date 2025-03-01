56fd77de07a8a9c1735fba3a5a71c94d289a5c29
Vaishnav Krishna P <124416940+VaishnavGitHuber@users.noreply.github.com>
2025-03-01 19:38:25 +0530

    Assignment 03 a and b

---
+++ PRACTICE QUESTIONS/EMPLOYEE/Program.cs
@@ -0,0 +1,34 @@
+﻿class Programe{
+    public static void Main(string[] args){
+        Employee emp1 = new Employee("Sunil", 1001,150000,10);
+        emp1.Display();
+        emp1.IncrementSalary(15000);
+        emp1.Display();
+    }
+}
+
+class Employee {
+    public string EmpName;
+    public int EmpId;
+    public double EmpSalary;
+    public int LeaveBalance;
+
+    // constructors 
+    public Employee(string EmpName, int EmpId, double EmpSalary, int LeaveBalance){
+        this.EmpName = EmpName;
+        this.EmpId = EmpId;
+        this.EmpSalary = EmpSalary;
+        this.LeaveBalance = LeaveBalance;
+    }
+
+    // increment salary 
+    public void IncrementSalary(double Amount){
+        this.EmpSalary += Amount;
+        Console.WriteLine($"Employee salary is incremented as: {EmpSalary}");
+    }
+
+    // display name- leave 
+    public void Display(){
+        Console.WriteLine($"Employee Name: {EmpName}  LeaveBalance: {LeaveBalance}");
+    }
+}
---
+++ PRACTICE QUESTIONS/TELECOM/Program.cs
@@ -0,0 +1,20 @@
+﻿class Programe {
+    public static void Main(string[] args){
+        TelecomDatabase tel = new TelecomDatabase();
+        tel.CustomerName = "Vaishnav KP";
+        tel.CustomerNumber = "+91 9765234323";
+        tel.CallUsage =  15;
+        tel.CalculateBillAmount();
+    }
+}
+public class TelecomDatabase {
+    public string CustomerName;
+    public string CustomerNumber;
+    public int CallUsage;
+
+    // Calculate the bill amount
+    public void CalculateBillAmount(){
+        double TotalAmount = CallUsage * 2; // 15 * 2 <= 30
+        Console.WriteLine($"Total Amount: {TotalAmount}");
+    }
+}
