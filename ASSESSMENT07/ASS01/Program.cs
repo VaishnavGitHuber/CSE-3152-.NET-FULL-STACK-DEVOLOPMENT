06b00280f00e96f42cd14d2e1422315c957f49f6
Vaishnav Krishna P <124416940+VaishnavGitHuber@users.noreply.github.com>
2025-04-03 19:58:45 +0530

    Assessment 07

---
+++ ASSESSMENT07/ASS01/Program.cs
@@ -0,0 +1,34 @@
+﻿/* 
+Write a C# Program to create 10 fruit list and later remove the last 3 names from the list.
+*/
+
+using System.Collections;
+
+class Programe {
+    public static void Main(string[] args){
+        // Create a datastructure : stack 
+        Stack stack = new Stack();
+
+        // adding items : end 
+        stack.Push("Apple");
+        stack.Push("Mango");
+        stack.Push("Orange");
+        stack.Push("Pineapple");
+        stack.Push("Guave");
+        stack.Push("Grapes");
+        stack.Push("Watermelon");
+        stack.Push("sweet Mango");
+        stack.Push("fruit9");
+        stack.Push("fruit10");
+
+        // removing the last 3 fruits 
+        stack.Pop();
+        stack.Pop();
+        stack.Pop();
+
+        // printint the remining 7 fruits 
+        foreach(var fruit in stack){
+            Console.Write(fruit + ", ");
+        }Console.WriteLine();
+    }
+}
---
+++ ASSESSMENT07/ASS02/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Write a C# program to create the sorted array of 5 students details with roll number and name.
+*/
+using System.Collections;
+class Programe {
+    public static void Main(string[] args){
+        // SortedList : Create a object of the Datastructure 
+        SortedList sortedlist = new SortedList();
+
+        // adding the 5 student name & rollnumber 
+        sortedlist.Add("Vaishnav Krishna P", "20221COM0040");
+        sortedlist.Add("Dhanush SN", "20221COM0032");
+        sortedlist.Add("Sunil Kumar Pandab", "20221COM0034");
+        sortedlist.Add("Madhu Praneeth", "20221COM0036");
+        sortedlist.Add("Danda Tarun Chaudary", "20221COM0055");
+
+        // printing : sorted list 
+        foreach(DictionaryEntry item in sortedlist){
+            Console.WriteLine($"Name: {item.Key} Value: {item.Value}");
+        }
+    }
+}
